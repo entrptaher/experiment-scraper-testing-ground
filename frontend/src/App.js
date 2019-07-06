bba3d848f80e7fd22d852e8e7856e3ce9a7917b5
Naimur <naimur@Naimurs-iMac.local>
2019-07-06 12:50:24 +0600

    feat: ajax with static data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-import Recaptcha from './Component/Re-Captcha';
-import Home from './Component/Home';
-import Navbar from './Component/Nav-bar';
-import TableReport from './Component/Table-Report';
+
+import Recaptcha from './component/Re-Captcha';
+import Home from './component/Home';
+import Navbar from './component/Nav-bar';
+import TableReport from './component/Table-Report';
+import Ajax from './component/Ajax';
+
 import './css/style.css';
 
 function App() {
@@ -22,6 +25,7 @@ function App() {
 				<Route exact path="/" component={Home} />
 				<Route path="/table" component={TableReport} />
 				<Route path="/recaptcha" component={Recaptcha} />
+				<Route path="/ajax" component={Ajax} />
 			</div>
 		</BrowserRouter>
 	);
