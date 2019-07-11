253c6508723fd8e8bffd495843f279956424c0e9
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2019-07-11 12:44:33 +0600

    fix: removes class based component where don't need

--- frontend/src/component/Re-Captcha.js
+++ frontend/src/component/Re-Captcha.js
@@ -5,67 +5,68 @@ import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 class ReCaptcha extends Component {
-	state = {
-		captcha: ''
-	};
+  state = {
+    captcha: ''
+  };
 
-	onChange = value => {
-		this.setState({
-			captcha: value
-		});
-	};
+  onChange = value => {
+    this.setState({
+      captcha: value
+    });
+  };
 
-	sendRequest = e => {
-		const { captcha } = this.state;
-		fetch(`${process.env.REACT_APP_SERVER}/subscribe`, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json, text/plain, */*',
-				'Content-type': 'application/json'
-			},
-			body: JSON.stringify({ captcha: captcha })
-		})
-			.then(res => res.json())
-			.then(data => {
-				if (data.success) {
-					toast.success(data.msg);
-				} else {
-					toast.error(data.msg);
-				}
-			});
-	};
+  sendRequest = e => {
+    const { captcha } = this.state;
+    fetch(`${process.env.REACT_APP_SERVER}/subscribe`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({ captcha: captcha })
+    })
+      .then(res => res.json())
+      .then(({ success, msg }) => {
+        if (success) {
+          toast.success(msg);
+        } else {
+          toast.error(msg);
+        }
+      });
+  };
 
-	render() {
-		return (
-			<div className="container">
-				<div className="recaptcha">
-					<h1 className="title">ReCAPTCHA</h1>
-					<hr className="line" />
-				</div>
+  render() {
+    const { captcha } = this.state;
+    return (
+      <div className="container">
+        <div className="recaptcha">
+          <h1 className="title">ReCAPTCHA</h1>
+          <hr className="line" />
+        </div>
 
-				<ReCAPTCHA
-					sitekey={process.env.REACT_APP_CAPTCHA_SITE_KEY}
-					onChange={this.onChange}
-				/>
+        <ReCAPTCHA
+          sitekey={process.env.REACT_APP_CAPTCHA_SITE_KEY}
+          onChange={this.onChange}
+        />
 
-				{this.state.captcha && (
-					<textarea
-						className="area"
-						name=""
-						id=""
-						cols="30"
-						rows="5"
-						readOnly
-						value={this.state.captcha}
-					/>
-				)}
+        {captcha && (
+          <textarea
+            className="area"
+            name=""
+            id=""
+            cols="30"
+            rows="5"
+            readOnly
+            value={captcha}
+          />
+        )}
 
-				<button onClick={this.sendRequest} className="submit">
-					Submit
-				</button>
-			</div>
-		);
-	}
+        <button onClick={this.sendRequest} className="submit">
+          Submit
+        </button>
+      </div>
+    );
+  }
 }
 
 export default ReCaptcha;
